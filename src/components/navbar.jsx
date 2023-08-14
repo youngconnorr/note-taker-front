dbbac9bc1148f59fc5eb4dec150c11a4f2fa1a68
conryxng <youngconnor2004@gmail.com>
2023-08-14 12:30:38 -0600

    added a front end to login and register

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,29 @@
 import { Link } from 'react-router-dom';
-
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom'
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+
+    const Logout = () => {
+        setCookies("access_token", "")
+        window.localStorage.removeItem("userID");
+
+    }
     return (
         <>
-        <nav className="navbar">
-             <div>
-                <ul className='list'>
-                    <button><Link to='/'> Home</Link></button>
-                    <button><Link to='/note-list'> Your Notes</Link></button>
-                    <button><Link to='/make-note'> Create Note</Link></button>
-                    <button><Link to='/auth'> Login/Register</Link></button>
+            <nav className="navbar">
+                <div>
+                    <ul className='list'>
+                        <button><Link to='/'> Home</Link></button>
+                        <button><Link to='/note-list'> Your Notes</Link></button>
+                        <button><Link to='/make-note'> Create Note </Link></button>
+                        {!cookies.access_token ? (<button><Link to='/auth'>
+                            Login/Register</Link></button>) : <button> Log out</button>}
 
-                </ul>
-            </div>   
-        </nav>
-        
+                    </ul>
+                </div>
+            </nav>
+
         </>
     )
 }
--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,10 +1,110 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom'
 
 export const Auth = () => {
     return (
-    <>
-        <h1>
-            Auth  
-        </h1>
-    </>
+        <>
+            <h1>
+                <Login />
+                <Register />
+            </h1>
+        </>
+    )
+}
+
+const Register = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("http:localhost:3001/auth/register", { username, password }) // object for the body of the post request (ie. user,pass). Recall router.post .... req.body
+            alert("registration complete! login now!")
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    return (
+        <Form
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            label="Register"
+            onSubmit={onSubmit}
+
+        //using props and components we don't have to repeat ourselves
+        />
+    )
+}
+
+const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [_, setCookies] = useCookies(["access_token"]) // check again
+
+    const navigate = useNavigate()
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            const response = await axios.post("http:localhost:3001/auth/login", { username, password });
+
+            setCookies("access_token", response.data.token); //grabbing jwt and userid
+            window.localStorage.setItem("userID", response.data.userID)
+
+            navigate("/") //when finishing it will redirect you
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    return (
+        <Form
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            label="Login"
+        //using props and components we don't have to repeat ourselves
+        />
+    )
+}
+
+const Form = ({
+    username,
+    setUsername,
+    password,
+    setPassword,
+    label,
+    onSubmit,
+}) => {
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <h1> {label} </h1>
+                <div>
+                    <label htmlFor='username'>Username: </label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor='password'>Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)} />
+                </div>
+            </form>
+            <button type="submit"> {label} </button>
+        </>
     )
 }
